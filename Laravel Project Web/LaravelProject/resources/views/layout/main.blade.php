c40cac58459c57df19d814eb403813d211c86b88
sarahsftri <105918982+sarahsftri@users.noreply.github.com>
2022-12-07 22:03:16 +0700

    update

---
+++ Laravel Project Web/LaravelProject/resources/views/layout/main.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <title>giant book store</title>
+
+  @include('partials.styles')
+</head>
+<body>
+
+@include('partials.headerUnregister')
+
+<main class="container mt-5">
+    @yield('content')
+</main>
+
+@include('partials.footer')
+
+@include('partials.script')
+
+</body>
+</html>
