c4fe4fb5e7ccbb86890616b2af840125814fafcc
sarahsftri <105918982+sarahsftri@users.noreply.github.com>
2022-12-07 21:48:46 +0700

    Update headerUnregister.blade.php

---
+++ Laravel Project Web/LaravelProject/resources/views/partials/headerUnregister.blade.php
@@ -0,0 +1,19 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="{{ route('pages.index') }}">Giant Book Supplier</a>
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="{{ route('pages.index') }}">Sho</a>
+            </li>
+            wFlix
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route(' ') }}">Register</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route(' ') }}">Login</a>
+              </li>
+          </ul>
+      </div>
+    </nav>
+  </header>
