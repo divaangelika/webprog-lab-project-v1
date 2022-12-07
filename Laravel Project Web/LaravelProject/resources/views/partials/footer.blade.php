727efe91d3104896dedc47fd0ef90ef28db7cb1f
sarahsftri <105918982+sarahsftri@users.noreply.github.com>
2022-12-07 21:06:45 +0700

    Create footer.blade.php

---
+++ Laravel Project Web/LaravelProject/resources/views/partials/footer.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Bootstrap 5 Social Media Icon Button</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- font asesome 6 cdn -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.0/css/all.css">
+  </head>
+
+  <body>
+    <div class="text-center">
+        <h1 class="display-3">ShowFlix</h1>
+    </div>
+    <div class="text-center">
+    <button class="btn btn-danger" type="button">
+      <i class="fa-brands fa-youtube"></i>
+    </button>
+    <button class="btn btn-info" type="button">
+      <i class="fa-brands fa-twitter text-white"></i>
+    </button>
+    <button class="btn btn-primary" type="button">
+      <i class="fa-brands fa-facebook"></i>
+    </button>
+    <button class="btn btn-danger" type="button">
+      <i class="fa-brands fa-instagram"></i>
+    </button>
+    <button class="btn btn-success" type="button">
+      <i class="fa-brands fa-whatsapp"></i>
+    </button>
+    <button class="btn btn-dark" type="button">
+      <i class="fa-brands fa-github"></i>
+    </button>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+    </div>
+    <div class="text-center">
+        &copy; {{ date('Y') }} ShowFlix All Right Reserved
+    </div>
+  </body>
+
+</html>
