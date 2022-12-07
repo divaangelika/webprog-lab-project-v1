13782a8149b2129f36a985e72c0c59f45d539509
divaangelika <diva.angelika.mulia@gmail.com>
2022-12-07 21:58:01 +0700

    Update web.php

--- Laravel Project Web/LaravelProject/routes/web.php
+++ Laravel Project Web/LaravelProject/routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/footer', function () {
     return view('partials.footer');
 });
+
+// Route::get('/footer', function () {
+//     return view('partials.footer');
+// });
 
 
