c907282dfc5384bef09ccf550e71774496ac4af1
sarahsftri <105918982+sarahsftri@users.noreply.github.com>
2022-12-27 14:14:23 +0700

    template carousel di home
    kasih template carousel di home

--- Laravel Project Web/LaravelProject/resources/views/layout/main.blade.php
+++ Laravel Project Web/LaravelProject/resources/views/layout/main.blade.php
@@ -4,13 +4,14 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="">
-  <title>giant book store</title>
+  <title>Showflix</title>
 
   @include('partials.styles')
 </head>
 <body>
 
     @include('partials.headerUnregister')
+    @include('partials.carousel')
 
 {{-- <main class="container mt-5">
     @yield('content')
