2ca348eb0aeaf7510f8a7d701ce25e08e6ddbc26
divaangelika <diva.angelika.mulia@gmail.com>
2022-12-27 13:53:03 +0700

    test

--- Laravel Project Web/LaravelProject/routes/web.php
+++ Laravel Project Web/LaravelProject/routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// asd
 
 Route::get('/', function () {
     return view('layout.main');
