d497fb8a41e395e879436435e6335f7429258879
sarahsftri <105918982+sarahsftri@users.noreply.github.com>
2023-01-06 06:45:01 +0700

    tampilan login and register

--- Laravel Project Web/LaravelProject/resources/views/login/login.blade.php
+++ Laravel Project Web/LaravelProject/resources/views/login/login.blade.php
@@ -4,35 +4,39 @@
   {{-- @include('partials.styles')
   @include('partials.headerUnregister') --}}
 
+<div class="mt-5" style="justify-content:center; align-items:center; display:flex;">
+    <div class="card w-50" style="background-color:rgb(175, 148, 217)">
+        <div class="card-body">
+            <form method="POST" action={{ url('/login')}}>
+                @csrf
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
 
-  <form method="POST" action={{ url('/login')}}>
-    @csrf
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                </div>
+                <div class="form-group form-check">
+                    <input type="checkbox" name="remember" class="form-check-input" id="remember" value="{{ Cookie::get('password_cookie') != null ? Cookie::get('password_cookie') : '' }}">
+                    <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+                </div>
 
-    <div class="form-group">
-        <label for="email">Email address</label>
-        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email">
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
     </div>
-    <div class="form-group">
-      <label for="password">Password</label>
-      <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-    </div>
-    <div class="form-group form-check">
-        <input type="checkbox" name="remember" class="form-check-input" id="remember" value="{{ Cookie::get('password_cookie') != null ? Cookie::get('password_cookie') : '' }}">
-        <label class="form-check-label" for="exampleCheck1">Remember Me</label>
-    </div>
-
-    <button type="submit" class="btn btn-primary">Login</button>
-  </form>
-
+</div>
 
 
 @include('partials.script')
--- Laravel Project Web/LaravelProject/resources/views/register/register.blade.php
+++ Laravel Project Web/LaravelProject/resources/views/register/register.blade.php
@@ -1,21 +1,26 @@
 @extends('master.master')
 @section('register')
 {{-- <h1>test123</h1> --}}
-
-<form method="POST" action={{ url('/register')}}>
-    @csrf
-    <div class="form-group">
-      <label for="username">Username</label>
-      <input type="username" class="form-control" id="username" name="username" aria-describedby="usernameHelp" placeholder="Enter Username">
+<div class="mt-5" style="justify-content:center; align-items:center; display:flex;">
+    <div class="card w-50" style="background-color:rgb(175, 148, 217)">
+        <div class="card-body">
+            <form method="POST" action={{ url('/register')}}>
+                @csrf
+                <div class="form-group">
+                <label for="username">Username</label>
+                <input type="username" class="form-control" id="username" name="username" aria-describedby="usernameHelp" placeholder="Enter Username">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-primary">Register</button>
+            </form>
+        </div>
     </div>
-    <div class="form-group">
-        <label for="email">Email address</label>
-        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email">
-      </div>
-    <div class="form-group">
-      <label for="password">Password</label>
-      <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-    </div>
-    <button type="submit" class="btn btn-primary">Register</button>
-  </form>
+</div>
 @endsection
