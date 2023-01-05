25a80ac48f1cb3d9469caa212a0569dfc09c09ab
sarahsftri <105918982+sarahsftri@users.noreply.github.com>
2023-01-06 01:52:09 +0700

    pusing

--- Laravel Project Web/LaravelProject/resources/views/login/login.blade.php
+++ Laravel Project Web/LaravelProject/resources/views/login/login.blade.php
@@ -5,7 +5,7 @@
   @include('partials.headerUnregister') --}}
 
 
-<form method="POST" action={{ url('/login')}}>
+  <form method="POST" action={{ url('/login')}}>
     @csrf
     @if ($errors->any())
     <div class="alert alert-danger">
@@ -18,12 +18,12 @@
     @endif
 
     <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <label for="email">Email address</label>
+        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
     </div>
     <div class="form-group">
-      <label for="exampleInputPassword1">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      <label for="password">Password</label>
+      <input type="password" class="form-control" id="password" placeholder="Password">
     </div>
     <div class="form-group form-check">
         <input type="checkbox" name="remember" class="form-check-input" id="remember" value="{{ Cookie::get('password_cookie') != null ? Cookie::get('password_cookie') : '' }}">
@@ -32,6 +32,7 @@
 
     <button type="submit" class="btn btn-primary">Login</button>
   </form>
+
 
 
 @include('partials.script')
--- Laravel Project Web/LaravelProject/routes/web.php
+++ Laravel Project Web/LaravelProject/routes/web.php
@@ -20,24 +20,18 @@ Route::get('/', function () {
 });
 
 # Route untuk Login
-// Route::get('/login', [AuthController::class, 'loginPage']);
-// Route::post('/login', [AuthController::class, 'loginMember']);
-
-// Route::get('/login', 'AuthController@loginPage');
-// Route::post('/login', 'AuthController@loginMember');
+Route::get('/login', [AuthController::class, 'loginPage']);
+Route::post('/login', [AuthController::class, 'loginMember']);
 
 # Route untuk Register
-// Route::get('/register', [AuthController::class, 'registerPage']);
-// Route::post('/register', [AuthController::class, 'registerMember']);
+Route::get('/register', [AuthController::class, 'registerPage']);
+Route::post('/register', [AuthController::class, 'registerMember']);
 
 
-// Route::get('/register', 'AuthController@registerPage');
-// Route::post('/register', 'AuthController@registerMember');
+// Route::get('/login', '\App\Http\Controllers\AuthController@loginPage');
+// Route::post('/login', '\App\Http\Controllers\AuthController@loginMember');
 
-Route::get('/login', '\App\Http\Controllers\AuthController@loginPage');
-Route::post('/login', '\App\Http\Controllers\AuthController@loginMember');
-
-Route::get('/register', '\App\Http\Controllers\AuthController@registerPage');
-Route::post('/register', '\App\Http\Controllers\AuthController@registerMember');
+// Route::get('/register', '\App\Http\Controllers\AuthController@registerPage');
+// Route::post('/register', '\App\Http\Controllers\AuthController@registerMember');
 
 
