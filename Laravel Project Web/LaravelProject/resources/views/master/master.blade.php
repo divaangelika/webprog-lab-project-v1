73dc0f0bdd7cafb06a6612d656e7221d550e1997
sarahsftri <105918982+sarahsftri@users.noreply.github.com>
2023-01-06 13:24:31 +0700

    Update master.blade.php

--- Laravel Project Web/LaravelProject/resources/views/master/master.blade.php
+++ Laravel Project Web/LaravelProject/resources/views/master/master.blade.php
@@ -12,36 +12,6 @@
 </head>
 <body>
 
-    {{-- <h1>test 123</h1> --}}
-
-    {{-- <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="">ShowFlix</a>
-            <ul class="nav justify-content-end">
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Movie</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Actor</a>
-                </li>
-                <button type="button"
-                    class="btn btn-outline-primary">
-                    <a href="/register">Register</a>
-
-                </button>
-                <button type="button"
-                    class="btn btn-outline-primary">
-                    <a href="/login">Login</a>
-                </button>
-              </ul>
-          </div>
-        </nav>
-      </header> --}}
-
       @yield('slider')
 
       @yield('register')
