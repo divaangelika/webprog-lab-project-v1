7103b792e5f81066b4b056126949a61843a79d37
sarahsftri <105918982+sarahsftri@users.noreply.github.com>
2023-01-07 11:28:39 +0700

    update

--- Laravel Project Web/LaravelProject/resources/views/homepage/homepage.blade.php
+++ Laravel Project Web/LaravelProject/resources/views/homepage/homepage.blade.php
@@ -14,22 +14,22 @@
       <div class="carousel-item active" data-bs-interval="10000">
         <img src="https://w0.peakpx.com/wallpaper/965/851/HD-wallpaper-avatar-2-2018-poster-fantasy-movie-avatar-2-navi-blue.jpg" class="d-block w-100" alt="..." width="500" height="600">
         <div class="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
+            <h5>Avatar: The Way of Water</h5>
+            <p>Jake Sully and Ney'tiri have formed a family and are doing everything to stay together. However, they must leave their home and explore the regions of Pandora. When an ancient threat resurfaces, Jake must fight a difficult war against the humans.</p>
         </div>
       </div>
       <div class="carousel-item" data-bs-interval="2000">
         <img src="https://wallpapercrafter.com/desktop/30567-Mission-Impossible-Fallout-poster-Tom-Cruise-4K-12K.jpg" class="d-block w-100" alt="..." width="500" height="600">
         <div class="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
+            <h5>Mission: Impossible Fallout</h5>
+            <p>A group of terrorists plans to detonate three plutonium cores for a simultaneous nuclear attack on different cities. Ethan Hunt, along with his IMF team, sets out to stop the carnage.</p>
         </div>
       </div>
       <div class="carousel-item">
         <img src="https://static1.moviewebimages.com/wordpress/wp-content/uploads/2022/09/wednesday-netflix-poster.jpeg" class="d-block w-100" alt="..." width="500" height="600">
         <div class="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
+            <h5>Wednesday</h5>
+            <p>While attending Nevermore Academy, Wednesday Addams attempts to master her emerging psychic ability, thwart a killing spree and solve the mystery that embroiled her parents 25 years ago.</p>
         </div>
       </div>
     </div>
--- Laravel Project Web/LaravelProject/resources/views/partials/headerAdmin.blade.php
+++ Laravel Project Web/LaravelProject/resources/views/partials/headerAdmin.blade.php
@@ -16,9 +16,6 @@
                 <a class="nav-link" href="">Add Movie</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="">Edit Movie</a>
-            </li>
-            <li class="nav-item">
                 <a class="nav-link" href="{{route('logout')}}">Log Out</a>
             </li>
           </ul>
