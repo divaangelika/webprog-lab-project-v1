746ca110c36d7911076bb79d633c85465164ef3d
sarahsftri <105918982+sarahsftri@users.noreply.github.com>
2023-01-09 16:58:41 +0700

    typo gais ternyata errornya

--- Laravel Project Web/LaravelProject/routes/web.php
+++ Laravel Project Web/LaravelProject/routes/web.php
@@ -19,7 +19,7 @@ Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // admin
 Route::get('/addMovie', [MovieController::class, 'addMoviePage'])->name('add-movie');
-Route::put('/addMovie', [MovieController::class, 'addMovie'])->name('add-movie-database');
+Route::post('/addMovie', [MovieController::class, 'addMovie'])->name('add-movie-database');
 
 
 // Route::get('/login', '\App\Http\Controllers\AuthController@loginPage');
