a444714fd26ae803a10338ae8cdf8dc7101e8ce2
sarahsftri <105918982+sarahsftri@users.noreply.github.com>
2023-01-10 20:48:50 +0700

    update

--- Laravel Project Web/LaravelProject/app/Models/Actor.php
+++ Laravel Project Web/LaravelProject/app/Models/Actor.php
@@ -5,10 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Genre extends Model
+class Actor extends Model
 {
-    public function genres()
-    {
-        return $this->hasMany(Movie::class);
-    }
+    use HasFactory;
 }
---
+++ Laravel Project Web/LaravelProject/database/migrations/2023_01_10_122836_actor.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Actor extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('gender');
+            $table->string('biography');
+            $table->date('dobActor');
+            $table->date('pobActor');
+            $table->string('imgActor');
+            $table->string('imgBackground');
+            $table->float('popularity');
+
+            // $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actors');
+    }
+}
