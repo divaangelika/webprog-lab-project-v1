a001bed5ea69ff6bbcaf49166c1619d363dde796
sarahsftri <105918982+sarahsftri@users.noreply.github.com>
2023-01-12 05:58:00 +0700

    Update homepage.blade.php

--- Laravel Project Web/LaravelProject/resources/views/homepage/homepage.blade.php
+++ Laravel Project Web/LaravelProject/resources/views/homepage/homepage.blade.php
@@ -8,7 +8,6 @@
 @include('partials.headerRegistered')
 @endif
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
 @section('slider')
 <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
