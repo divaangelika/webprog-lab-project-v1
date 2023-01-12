4bddf2e36c333f8d33465da70e9df5e40ab98704
Diva Angelika Mulia <diva.angelika.mulia@gmail.com>
2023-01-12 07:26:08 +0700

    update migration

--- Laravel Project Web/LaravelProject/database/migrations/2023_01_04_044002_create_genres_table.php
+++ Laravel Project Web/LaravelProject/database/migrations/2023_01_04_044002_create_genres_table.php
@@ -5,7 +5,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreateGenresTable extends Migration
-return new class extends Migration
 {
     /**
      * Run the migrations.
--- Laravel Project Web/LaravelProject/database/migrations/2023_01_10_122836_create_actors_table.php
+++ Laravel Project Web/LaravelProject/database/migrations/2023_01_10_122836_create_actors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Actor extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -37,3 +37,44 @@ class Actor extends Migration
         Schema::dropIfExists('actors');
     }
 };
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('gender');
+            $table->string('biography');
+            $table->date('dobActor');
+            $table->date('pobActor');
+            $table->string('imgActor');
+            $table->string('imgBackground');
+            $table->float('popularity');
+
+            // $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actors');
+    }
+}
