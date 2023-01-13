533cc279447d192358d58bc3002e3df0d73e1785
Diva Angelika Mulia <diva.angelika.mulia@gmail.com>
2023-01-13 12:43:52 +0700

    update storage buat image actor

--- Laravel Project Web/LaravelProject/resources/views/actor/actorData.blade.php
+++ Laravel Project Web/LaravelProject/resources/views/actor/actorData.blade.php
@@ -1,7 +1,7 @@
 @foreach ($actors as $actor)
     <a href="{{ url('actors/detail/' . $actor->id) }}" class="text-decoration-none">
         <div class="card m-2" style="width: 18rem;">
-            <img class="card-img-top px-3 pt-3 bg-dark" src="{{ url('/storage/actor/' . $actor->imgActor) }}"
+            <img class="card-img-top px-3 pt-3 bg-dark" src="{{ url('/storage/public/actor/' . $actor->imgActor) }}"
                 alt="Card image cap" style="z-index:1;  height:30vh; object-fit: cover;">
             <div class="card-body bg-dark">
                 <p class="card-text font-weight-bold text-white">{{ $actor->name }}</p>
